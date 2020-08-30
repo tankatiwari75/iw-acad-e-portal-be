fc0b160bdd8a4c279a1a08cf87f9f9fcc22fd354
dipak7541 <tiwaritanka75@gmail.com>
2020-08-30 14:08:54 +0545

    fix: fix the model attendance upload

--- e_portal_system/teachers/models.py
+++ e_portal_system/teachers/models.py
@@ -4,12 +4,11 @@ from django.utils import timezone
 
 
 class AttendanceUploads(models.Model):
-    student_name=models.ForeignKey(StudentRegistration,related_name="student", on_delete=models.CASCADE)
-    teacher_name= models.ForeignKey(TeacherRegistration, related_name="teacher", on_delete=models.CASCADE)
-    subject_name= models.ForeignKey(AddSubject, related_name="subject", on_delete=models.CASCADE)
-    student_id= models.ForeignKey(StudentRegistration, related_name="id22", on_delete=models.CASCADE)
-    attendance=  models.BooleanField(default=False)
-    date= models.DateField(default=timezone.now)
+    student_id=models.ForeignKey(StudentRegistration,related_name="student", on_delete=models.CASCADE)
+    teacher_id= models.ForeignKey(TeacherRegistration, related_name="teacher", on_delete=models.CASCADE)
+    subject_name= models.ForeignKey(AddSubject, to_field="subject_name", on_delete=models.CASCADE)
+    class_held= models.IntegerField(default=0)
+    class_present = models.IntegerField(default=0)
 
 class ResultUpload(models.Model):
     student_name=models.ForeignKey(StudentRegistration,related_name="students", on_delete=models.CASCADE)
