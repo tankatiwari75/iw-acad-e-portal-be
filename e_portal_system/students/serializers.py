2da184b402f6a78dc699e4c4bbc1e5705d529fd4
dipak7541 <tiwaritanka75@gmail.com>
2020-09-11 19:41:57 +0545

    Syllabus Serializers

---
+++ e_portal_system/students/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from adminsite.models import AddSubject
+
+
+class SyllabusViewModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AddSubject 
+        fields = [
+            'class_number',
+            'subject_name',
+            'subject_syllabus',
+        ]  
--- e_portal_system/students/views.py
+++ e_portal_system/students/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
 # Create your views here
+from django.shortcuts import render
+
+# Create your views herefrom django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from .serializers import SyllabusViewModelSerializer
+#from adminsite.serializers import GetStudentSerializers, FetchSubject, CreateSubjectSerializers
+from adminsite.models import AddSubject
+
+
+class SyllabusView(viewsets.ViewSet):
+
+    def list(self, request, class_number):
+        queryset = AddSubject.objects.filter(class_number = class_number)
+        serializer = SyllabusViewModelSerializer(queryset, many = True) 
+        #serializer = CreateSubjectSerializers(queryset, many = True) 
+        return Response(serializer.data)
