82a62d78577c4209a122cecd356c9d55477595aa
dipak7541 <tiwaritanka75@gmail.com>
2020-09-13 03:47:48 +0545

    add:Procifle for deploy in heroku

--- e_portal_system/e_portal_system/settings.py
+++ e_portal_system/e_portal_system/settings.py
@@ -119,20 +119,25 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_L10N = True
+django_heroku.settings(locals())
 
 USE_TZ = True
 
 
 #Cors origin allow all will provide access to all headers
 CORS_ORIGIN_ALLOW_ALL = True
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
+
 AUTH_USER_MODEL="adminsite.AddUser"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
+
